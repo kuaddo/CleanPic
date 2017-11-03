3b3e08399228f8c1e04d1cbbb80c112ee600f8d4
shiita0903 <shiita0903@gmail.com>
2017-11-03 23:17:47 +0900

    APIテストページ追加

--- CleaningPic/CleaningPic/CleaningPic/Views/MainPageDetail.xaml.cs
+++ CleaningPic/CleaningPic/CleaningPic/Views/MainPageDetail.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +12,44 @@ namespace CleaningPic.Views
 {
     public partial class MainPageDetail : ContentPage
     {
+        private const string API = "http://35.189.147.104/api/";
+        private static HttpClient client = new HttpClient() { Timeout = TimeSpan.FromMilliseconds(3000) };
+
         public MainPageDetail()
         {
             InitializeComponent();
+        }
+
+        public async void GetButton_Clicked(object sender, EventArgs e)
+        {
+            if (apiPicker.SelectedIndex == -1) return;
+
+            apiPicker.IsEnabled = false;
+            getButton.IsEnabled = false;
+            indicator.IsVisible = true;
+            HttpResponseMessage result = null;
+
+            try
+            {
+                result = await client.GetAsync(API + apiPicker.SelectedItem.ToString());
+            }
+            catch (TaskCanceledException _)
+            {
+                resultLabel.Text = "タイムアウト";
+                indicator.IsVisible = false;
+                apiPicker.IsEnabled = true;
+                getButton.IsEnabled = true;
+                return;
+            }
+
+            if (result?.StatusCode == System.Net.HttpStatusCode.OK)
+                resultLabel.Text = await result.Content.ReadAsStringAsync();
+            else
+                resultLabel.Text = "通信失敗";
+
+            indicator.IsVisible = false;
+            apiPicker.IsEnabled = true;
+            getButton.IsEnabled = true;
         }
     }
 }
